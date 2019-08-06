fde9e0be2943423e19a5bd9f1ef1b91a8c7057ef
duydo <duydole>
2019-08-06 14:36:36 +0700

    First commit

---
+++ ZAMediaPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZAMediaPlayer
+//
+//  Created by CPU11996 on 8/6/19.
+//  Copyright © 2019 vng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
